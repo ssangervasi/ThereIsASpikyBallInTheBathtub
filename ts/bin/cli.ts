aec51fbdad919aefbd0528800259bff025039788
Sebastian Sangervasi <villain@harmless.dev>
2021-05-27 12:51:06 -0700

    oh node

--- ts/bin/cli.ts
+++ ts/bin/cli.ts
@@ -1,132 +1,45 @@
-#!/usr/bin/env ts-node
 import { Command } from 'commander'
-import WebSocket from 'ws'
 
-import { guardEcho } from '@sangervasi/common/dist/messages/echo'
-import {
-	guardJoin,
-	guardJoined,
-	guardPoint,
-} from '@sangervasi/common/dist/messages/gnop'
-import {
-	guardCreated,
-} from '@sangervasi/common/dist/messages/session'
-import {
-	parseMessage,
-} from '@sangervasi/common/dist/messages/index'
-
-const PORT = Number(process.env.PORT) || 42992
+import Gnop from '../src/gnop'
 
 const program = new Command()
-program.option('--host <type>', 'Host', 'local')
+program
+	.option('--host <type>', 'Host', 'local')
+	.option('--port <port>', 'Port', '42992')
 
-const conn = async (options: Record<string, unknown>) => {
-	const host =
-		options.host === 'remote' ? 'ws://sangervasi.net' : `ws://localhost:${PORT}`
-
-	console.log('Connecting')
-	const ws = new WebSocket(host)
-
-	ws.onopen = () => { console.log('Opened') }
-	ws.onclose = () => { console.log('Closed') }
-	ws.onerror = () => { console.log('Errored') }
-
-	const p = new Promise<string>((resolve) => {
-		const handleMessage = (event: { data: string }) => {
-			const message = parseMessage(event.data)
-
-			if (
-				guardCreated.message(message)
-			) {
-				console.log('Connected', message.sessionUuid)
-				ws.removeEventListener('message', handleMessage)
-				resolve(message.sessionUuid)
-			} else {
-				console.log('Expected session, got:', message)
-			}
-		}
-
-		ws.addEventListener('message', handleMessage)
-	})
-
-	const sessionUuid = await p
-	return { ws, sessionUuid }
+const getHost = (options: Record<string, unknown>) => {
+	return options.host === 'remote' ? 'ws://sangervasi.net' : `ws://localhost:${options.port}`
 }
 
 program
 	.command('echo')
 	.action(async (options) => {
-		const { ws, sessionUuid } = await conn(options)
+		const client = new Gnop.WsClient({
+			host: getHost(options),
+			name: "Echo client"
+		})
+
+		await client.connect()
 
 		setInterval(() => {
-			console.log('client speaking:')
-			const m = guardEcho.build({
-				type: 'echo',
-				sessionUuid,
-				payload: new Date().toISOString(),
-			})
-			ws.send(JSON.stringify(m))
+			client.speak()
 		}, 2000)
-
-		ws.addEventListener('message', (event: { data: string }) => {
-			const message = parseMessage(event.data)
-
-			if (guardEcho.message(message)) {
-				console.log('client heard:', message)
-				return
-			}
-		})
 	})
 
-const join = async (c: { ws: WebSocket; sessionUuid: string }, name: string) => {
-	console.log('Joining', c.sessionUuid)
-	const mJoin = guardJoin.build({
-		type: 'gnop.join',
-		sessionUuid: c.sessionUuid,
-		payload: {
-			name,
-		},
-	})
-
-	c.ws.send(JSON.stringify(mJoin))
-
-	await new Promise((resolve) => {
-		const handleJoined = (event: { data: string }) => {
-			const message = parseMessage(event.data)
-			if (guardJoined.message(message)) {
-				console.log('Joined', c.sessionUuid)
-				c.ws.removeEventListener('message', handleJoined)
-				resolve(null)
-			} else {
-				console.log('Expected joined, got:', message)
-			}
-		}
-		c.ws.addEventListener('message', handleJoined)
-	})
-}
 
 program
 	.command('point')
 	.action(async (options) => {
-		const c = await conn(options)
-		await join(c, 'Send points')
-
-		const s = (x: number, y: number) => {
-			const mPoint = guardPoint.build({
-				type: 'gnop.point',
-				sessionUuid: c.sessionUuid,
-				payload: {
-					x: x,
-					y: y,
-				},
-			})
-			console.log('Sending', mPoint)
-			c.ws.send(JSON.stringify(mPoint))
-		}
+		const client = new Gnop.WsClient({
+			host: getHost(options),
+			name: "Point client"
+		})
+		await client.connect()
+		await client.join()
 
 		for (let i = 0; i < 10; i++) {
 			setTimeout(() => {
-				s(i, i)
+				client.point(i, i)
 			}, i * 1000)
 		}
 
@@ -136,24 +49,18 @@ program
 			}, 11 * 1000)
 		})
 
-		c.ws.close()
+		client.ws?.close()
 	})
 
 program
 	.command('listen <name>')
 	.action(async (name: string, options) => {
-		const c = await conn(options)
-		await join(c, 'Listener')
-
-		c.ws.addEventListener('message', (event: { data: string }) => {
-			const message = parseMessage(event.data)
-
-			if (guardPoint.message(message)) {
-				console.log('Received point:', message.payload)
-			} else {
-				console.log('Expected point, received:', message)
-			}
+		const client = new Gnop.WsClient({
+			host: getHost(options),
+			name: "Listen client"
 		})
+		await client.connect()
+		await client.join()
 	})
 
 program.parse(process.argv)
--- ts/src/gnop.ts
+++ ts/src/gnop.ts
@@ -1,28 +1,201 @@
-// import { fmtJSON } from "@sangervasi/common/dist/utils/fmt"
-// import { guardJoin } from "@sangervasi/common/dist/messages/gnop"
-import { fmtJSON } from "@sangervasi/common/lib/utils/fmt"
-import { guardJoin } from "@sangervasi/common/lib/messages/gnop"
-// import { fmtJSON } from "@sangervasi/common/utils/fmt"
-// import { guardJoin } from "@sangervasi/common/messages/gnop"
+import WebSocket from 'ws'
+
+import { guardEcho } from '@sangervasi/common/lib/messages/echo'
+import {
+	guardJoin,
+	guardJoined,
+	guardPoint,
+} from '@sangervasi/common/lib/messages/gnop'
+import { guardCreated } from '@sangervasi/common/lib/messages/session'
+import { parseMessage } from '@sangervasi/common/lib/messages/index'
+
+import { fmtJSON } from '@sangervasi/common/lib/utils/fmt'
+
+class WsClient {
+	ws?: WebSocket
+	sessionUuid?: string
+	state: "init" | "creating" | "created" | "joining" | "joined" = "init"
+	
+	promises: {
+		connect?: {
+			promise: Promise<unknown>
+			resolve: (value?: unknown) => void
+		}
+		join?: {
+			promise: Promise<unknown>
+			resolve: (value?: unknown) => void
+		}
+	} = {}
+
+	constructor(
+		public options: {
+			host: string
+			name?: string
+		},
+	) { }
+
+	async connect() {
+		console.log('Connecting')
+		const ws = new WebSocket(this.options.host)
+		this.ws = ws
+
+		ws.onopen = this.handleOpen
+		ws.onclose = this.handleClose
+		ws.onerror = this.handleError
+		ws.addEventListener('message', this.handleMessage)
+
+		this.state = "creating"
+
+		const promise = new Promise((resolve) => {
+			this.promises.connect = {
+				promise,
+				resolve
+			}
+		})
+
+		return promise
+	}
+
+	handleMessage = (event: { data: string }) => {
+		const message = parseMessage(event.data)
+
+		if (this.state === "creating") {
+			if (guardCreated.message(message)) {
+				console.info('Connected', message.sessionUuid)
+				this.sessionUuid = message.sessionUuid
+				this.promises.connect?.resolve()
+			} else {
+				console.warn('Expected created message, got:', message)
+			}
+			return
+		}
+
+		if (this.state === "joining") {
+			if (guardJoined.message(message)) {
+				console.log('Joined', this.sessionUuid)
+				this.state = "joined"
+				this.promises.join?.resolve()
+			} else {
+				console.warn('Expected joined message, got:', message)
+			}
+			return
+		}
+
+		if (guardEcho.message(message)) {
+			console.log('client heard:', message)
+			return
+		}
+
+		console.warn('Unhandled messag:', message)
+
+	}
+
+	handleOpen = () => {
+		console.info('Opened')
+	}
+
+	handleClose = () => {
+		this.state = 'init'
+		console.info('Closed')
+	}
+
+	handleError = () => {
+		this.state = 'init'
+		console.info('Errored')
+	}
+
+	isReady(): this is { sessionUuid: string, ws: WebSocket } {
+		return Boolean(this.sessionUuid)
+			&& Boolean(this.ws)
+			&& this.state === "created"
+	}
+
+	async join() {
+		if (!this.isReady()) {
+			console.warn('Cannot join from state:', this.state)
+			return
+		}
+		
+
+		const mJoin = guardJoin.build({
+			type: 'gnop.join',
+			sessionUuid: this.sessionUuid,
+			payload: {
+				name: this.options.name || "Name",
+			},
+		})
+
+		console.info('Client joining:', mJoin)
+		this.state = "joining"
+
+		this.ws.send(JSON.stringify(mJoin))
+
+		const promise = new Promise((resolve) => {
+			this.promises.join = {
+				promise,
+				resolve
+			}
+		})
+		return promise
+	}
+
+	speak() {
+		if (!this.isReady()) {
+			console.warn('Cannot speak from state:', this.state)
+			return
+		}
+
+		const m = guardEcho.build({
+			type: 'echo',
+			sessionUuid: this.sessionUuid,
+			payload: new Date().toISOString(),
+		})
+
+		console.info('Client speaking:', m)
+
+		this.ws.send(JSON.stringify(m))
+	}
+
+	point(x: number, y: number) {
+		if (!this.isReady()) {
+			console.warn('Cannot send point from state:', this.state)
+			return
+		}
+
+		const mPoint = guardPoint.build({
+			type: 'gnop.point',
+			sessionUuid: this.sessionUuid,
+			payload: {
+				x: x,
+				y: y,
+			},
+		})
+
+		console.info('Client sending point:', mPoint)
+
+		this.ws.send(JSON.stringify(mPoint))
+	}
+}
+
 
 const hello = () => {
 	const m = guardJoin.build({
-		type: "gnop.join",
-		sessionUuid: "asdf",
+		type: 'gnop.join',
+		sessionUuid: 'asdf',
 		payload: {
-			name: "Jorsh"
-		}
+			name: 'Jorsh',
+		},
 	})
 	return fmtJSON(m)
 }
 
-
 const Gnop = {
-	hello
+	hello,
+	WsClient
 } as const
 
-Object.assign(
-	globalThis, {
-		Gnop
-	}
-)
+export default Gnop
+
+Object.assign(globalThis, {
+	Gnop,
+})
