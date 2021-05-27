efcf7bfbc1fd7cc4c1fecd8934f99d902d3d5d70
Sebastian Sangervasi <villain@harmless.dev>
2021-05-26 17:56:34 -0700

    never didn't have it

--- ts/src/gnop.ts
+++ ts/src/gnop.ts
@@ -1,9 +1,11 @@
-import { fmtJSON } from "@sangervasi/common/dist/utils/fmt"
-import {
-	guardJoin
-} from "@sangervasi/common/dist/messages/gnop"
+// import { fmtJSON } from "@sangervasi/common/dist/utils/fmt"
+// import { guardJoin } from "@sangervasi/common/dist/messages/gnop"
+import { fmtJSON } from "@sangervasi/common/lib/utils/fmt"
+import { guardJoin } from "@sangervasi/common/lib/messages/gnop"
+// import { fmtJSON } from "@sangervasi/common/utils/fmt"
+// import { guardJoin } from "@sangervasi/common/messages/gnop"
 
-export const hello = () => {
+const hello = () => {
 	const m = guardJoin.build({
 		type: "gnop.join",
 		sessionUuid: "asdf",
